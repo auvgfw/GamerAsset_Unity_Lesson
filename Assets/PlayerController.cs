ba3c6ea0f0e837823e4d3c145f97c006008234ff
Zou Lance <double_again@hotmail.com>
2020-10-07 17:54:39 +0800

    Make a 3D scratch templete.
    The cat can go for 1 grid with the function go();
    turn right with 90 degree with the function turn();

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform OriginalPos;
+    GameObject Animal;
+    Vector3 nextpos,nextrot;
+    Sequence mySequence;
+    void Start()
+    {
+        Animal = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnGoButton()
+    {
+        nextpos = transform.position;
+        nextrot = transform.eulerAngles;
+        mySequence = DOTween.Sequence();
+
+        int i;
+        Go();
+        for (i = 0; i < 10; i++)
+        {
+            
+            Turn();
+            Go();
+        }
+    }
+    
+    
+    void Go()
+    {
+        if (nextrot.y == 0)
+        {
+            nextpos.z++;
+        }
+        if (nextrot.y == 90)
+        {
+            nextpos.x++;
+        }
+        if (nextrot.y == 180)
+        {
+            nextpos.z--;
+        }
+        if (nextrot.y == 270)
+        {
+            nextpos.x--;
+        }
+        mySequence.Append(transform.DOMove(nextpos,1));
+    }
+    void Turn()
+    {
+        if (nextrot.y == 0)
+        {
+            nextrot.y = 90;
+        }
+        else if (nextrot.y == 90)
+        {
+            nextrot.y=180;
+        }
+        else if (nextrot.y == 180)
+        {
+            nextrot.y=270;
+        }
+        else if (nextrot.y == 270)
+        {
+            nextrot.y=0;
+        }
+        mySequence.Append(transform.DORotate(nextrot, 1));
+    }
+    void ResetPos()
+    {
+        transform.position = OriginalPos.position;
+        transform.rotation = OriginalPos.rotation;
+    }
+
+}
---
+++ Assets/WorldPlacer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldPlacer : MonoBehaviour
+{
+    public GameObject[] cubeStyles;
+    public Transform startPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i, j;
+        for (i = 0; i < 19; i++)
+        {
+            for (j = 0; j < 19; j++)
+            {
+                Vector3 tempTrans;
+                tempTrans = startPoint.position;
+                tempTrans.x += i;
+                tempTrans.z += j;
+                int style = ((int)(Random.value * 10.0f)) % 2+1;
+                GameObject cube_temp=Instantiate(cubeStyles[style], tempTrans,startPoint.rotation);
+                cube_temp.transform.parent = this.transform;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
